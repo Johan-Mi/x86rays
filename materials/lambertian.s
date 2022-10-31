70f435ae181af38022d88993986ac89d7f9b7aae
Johan-Mi <johanmi@protonmail.com>
2022-10-31 21:59:06 +0100

    Fix a bug in lambertian scattering

--- materials/lambertian.s
+++ materials/lambertian.s
@@ -1,7 +1,7 @@
 scatter_lambertian:
     call random_unit_vector
     addps xmm0, [hit_normal]
-    dpps xmm1, xmm0, 0b01110001
+    vdpps xmm1, xmm0, xmm0, 0b01110001
     ucomiss xmm1, [.too_close_to_zero]
     ja .done
     movaps xmm0, [hit_normal]
