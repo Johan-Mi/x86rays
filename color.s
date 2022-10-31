b0b973606db9f0e1580956e7bf0263e6d8298df9
Johan-Mi <johanmi@protonmail.com>
2022-10-31 10:29:26 +0100

    Implement `log2ps` and `fexp2` functions
    These will be used for proper gamma correction.

--- color.s
+++ color.s
@@ -19,3 +19,36 @@ gamma_correct:
 .f255: dd 255.0
 align 16
 .shuffle: dq 0x8080808080080400, 0x8080808080808080
+
+log2ps:
+    vpsrld xmm1, xmm0, 23
+    vpbroadcastd xmm2, [.exponent_bias]
+    psubd xmm1, xmm2
+    pslld xmm0, 9
+    psrld xmm0, 9
+    vpbroadcastd xmm2, [.leading_one]
+    por xmm0, xmm2
+    cvtdq2ps xmm0, xmm0
+    cvtdq2ps xmm1, xmm1
+    mulps xmm0, xmm1
+    vbroadcastss xmm1, [.mantissa_scale]
+    divps xmm0, xmm1
+    ret
+align 4
+.exponent_bias: dd 127
+.leading_one: dd 1 << 24
+.mantissa_scale: dd eval(1 << 24) %+ .0
+
+fexp2:
+    fstcw [rsp-2]
+    or word [rsp-2], 0b11 << 10 ; Round towards zero
+    fldcw [rsp-2]
+    fld st0
+    fld st0
+    frndint
+    fsubp
+    f2xm1
+    fld1
+    faddp
+    fscale
+    fstp st1
