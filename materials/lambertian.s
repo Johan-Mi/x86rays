7c88ba8d90fc8df73315bc244a3666445ea5822a
Johan-Mi <johanmi@protonmail.com>
2022-10-31 21:41:33 +0100

    Fix lambertian scattering close to zero

--- materials/lambertian.s
+++ materials/lambertian.s
@@ -1,5 +1,11 @@
 scatter_lambertian:
     call random_unit_vector
     addps xmm0, [hit_normal]
-    ; TODO: Replace directions that are too close to zero
+    dpps xmm1, xmm0, 0b01110001
+    ucomiss xmm1, [.too_close_to_zero]
+    ja .done
+    movaps xmm0, [hit_normal]
+.done:
     ret
+align 4
+.too_close_to_zero: dd 3e-10
