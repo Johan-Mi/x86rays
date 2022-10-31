bee60262f8566572afdbfcab5e0d276578d8bbf8
Johan-Mi <johanmi@protonmail.com>
2022-10-31 22:39:05 +0100

    Proper gamma correction

--- color.s
+++ color.s
@@ -16,11 +16,38 @@ gamma_correct:
     fstp dword [rsp+8]
     movaps xmm0, [rsp]
     add rsp, 24
+    vbroadcastss xmm4, [.f1]
+    movss xmm5, [.f1]
+    vdpps xmm1, xmm0, [.luma_coefficients], 0b01110111
+    vsubps xmm3, xmm1, xmm0
+    vsubps xmm2, xmm1, xmm4
+    divps xmm2, xmm3
+    vcmpgtps xmm3, xmm0, xmm4
+    movd eax, xmm3
+    test al, al
+    jz .dont_clamp_r
+    minss xmm5, xmm2
+.dont_clamp_r:
+    extractps eax, xmm3, 1
+    test al, al
+    jz .dont_clamp_g
+    shufps xmm6, xmm2, 0b01010101
+    minss xmm5, xmm6
+.dont_clamp_g:
+    extractps eax, xmm3, 2
+    test al, al
+    jz .dont_clamp_b
+    shufps xmm6, xmm2, 0b10101010
+    minss xmm5, xmm6
+.dont_clamp_b:
+    vbroadcastss xmm5, xmm5
+    subps xmm0, xmm1
+    mulps xmm0, xmm5
+    addps xmm0, xmm1
     ; Clamp each channel between 0 and 1
     pxor xmm1, xmm1
     maxps xmm0, xmm1
-    vbroadcastss xmm1, [.f1]
-    minps xmm0, xmm1
+    minps xmm0, xmm4
     ; Scale up to 0-255
     vbroadcastss xmm1, [.f255]
     mulps xmm0, xmm1
@@ -33,6 +60,7 @@ gamma_correct:
 .f255: dd 255.0
 align 16
 .shuffle: dq 0x8080808080080400, 0x8080808080808080
+.luma_coefficients: dd 0.299, 0.587, 0.114, 0.0
 
 log2ps:
     vpsrld xmm1, xmm0, 23
